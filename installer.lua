ca7fd1c89d25bddd877c496663ff9191bfb4a838
hzFishy <84443782+hzFishy@users.noreply.github.com>
2023-08-03 16:03:20 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,4 +1,3 @@
-
 local files = {
   "StructureCCloner/modules/building/init.lua",
   "StructureCCloner/modules/inventory/init.lua",
@@ -18,18 +17,53 @@ local dirs = {
   "StructureCCloner/userData"
 }
 
-local text = "downloading "
-local endtext = "/"..#files
 
+local starttext = "downloading "
+local endtext = "/"..#files
 local function init()
-  print(text.."0"..endtext)
+  print(starttext.."0"..endtext)
 end
 local x,y = term.getCursorPos()
 init()
 local function update(i)
     term.setCursorPos(x,y)
-    print(text..i..endtext)
+    print(starttext..i..endtext)
 end
+
+local function ternary(condition, value_if_true, value_if_false)
+  if condition then
+      return value_if_true
+  else
+      return value_if_false
+  end
+end
+local function resetColor()
+  term.setTextColor(colors.white)
+end
+local function changeColor(v)
+  term.setTextColor(v)
+end
+local function splitWrite(tbltext,tblcolors,skipline)
+  skipline = ternary(skipline == nil, true, skipline)
+  for i, text in ipairs(tbltext) do
+      local color = tblcolors[i]
+      if color then
+          changeColor(color)
+      else
+          changeColor(colors.white)
+      end
+      write(text)
+  end
+  if skipline then
+      print()
+  end
+  resetColor()
+end
+local function askInput()
+  Term.resetColor()
+  write("> ")
+end
+
 
 local url = "https://raw.githubusercontent.com/hzFishy/StructureCCloner/main/src/"
 local tasks = {}
@@ -56,29 +90,27 @@ end
 print("=== Starting instalation ===")
 parallel.waitForAll(table.unpack(tasks))
 
-local termm = require "/StructureCCloner.modules.term"
-Utils = require "/StructureCCloner.modules.utils"
+
 local completion = require "cc.completion"
 
-termm.changeColor(colors.red)
+changeColor(colors.red)
 shell.run("set motd.enable false")
-termm.changeColor(colors.orange)
+changeColor(colors.orange)
 print("Disabled motd, do 'set motd.enable true' to set back to enable")
 
 
 io.open("StructureCCloner.lua", "w"):write('shell.run("StructureCCloner/init.lua")'):close()
 print("StructureCCloner successfully installed!")
-termm.splitWrite({"Run ","/StructureCCloner.lua"," to start."},{nil,colors.blue,nil})
+splitWrite({"Run ","/StructureCCloner.lua"," to start."},{nil,colors.blue,nil})
 
 
-termm.changeColor(colors.orange)
+changeColor(colors.orange)
 print("Create a startup file for StructureCCloner ?")
-termm.askInput()
+askInput()
 local ans = select(1,(string.gsub(read(nil, {}, function(text) return completion.choice(text, {"yes","no"}) end)," ","")))
 if ans == "yes" then
     io.open("startup.lua", "w"):write('shell.run("StructureCCloner.lua")'):close()
 end
 
-termm.changeColor(colors.green)
+changeColor(colors.green)
 print("=== Instalation complete ===")
-termm, completion = nil, nil
